bda7e900c2a93c7e6d34ff978c2323abe64837d1
ssonsh <chosunci@gmail.com>
2022-03-12 20:38:54 +0900

    first commit

---
+++ src/main/java/com/ssonsh/studycloudfunctions/StudyCloudFunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.ssonsh.studycloudfunctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCloudFunctionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCloudFunctionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssonsh/studycloudfunctions/StudyCloudFunctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssonsh.studycloudfunctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCloudFunctionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
